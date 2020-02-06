5312952f2edab90796f345df41dc42668d4653f3
Scott <sacker@mail.bradley.edu>
2020-02-06 15:50:13 -0600

    Some Camera stuff

---
+++ AlterHeart/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿/*****************************************************************************
+// File Name: CameraController.cs
+// Author:
+// Creation Date: 2/6/2020
+//
+// Brief Description:
+*****************************************************************************/
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
