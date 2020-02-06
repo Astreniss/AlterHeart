6883c16a09cc8cf6873344ef6b6bc92203ef2ec9
Scott <sacker@mail.bradley.edu>
2020-02-06 15:35:37 -0600

    Player Script and Subfolders

---
+++ AlterHeart/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,48 @@
+﻿/*****************************************************************************
+// File Name: PlayerBehaviour.cs
+// Author:
+// Creation Date: 2/6/2020
+//
+// Brief Description:
+*****************************************************************************/
+
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    private void Update()
+    {
+        Movement();
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    private void Movement()
+    {
+
+    }
+
+    public void Jump()
+    {
+        if (OnGround())
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+
+    }
+
+    private bool OnGround()
+    {
+        return true;
+    }
+}
---
+++ AlterHeart/Assets/Scripts/RealitySwitcher.cs
@@ -0,0 +1,57 @@
+﻿/*****************************************************************************
+// File Name: RealitySwitcher.cs
+// Author:
+// Creation Date: 2/6/2020
+//
+// Brief Description:
+*****************************************************************************/
+
+using System.Collections;
+using UnityEngine;
+
+public class RealitySwitcher : MonoBehaviour
+{
+    public GameObject realityOne;
+    public GameObject realityTwo;
+    public GameObject player;
+
+    private int currentReality;
+
+    private void Start()
+    {
+        currentReality = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Swap Realities"))
+        {
+            StartCoroutine(SwitchReality());
+        }
+
+    }
+
+    private IEnumerator SwitchReality()
+    {
+        player.GetComponent<PlayerBehaviour>().Jump();
+        yield return new WaitForSeconds(.2f);
+        player.GetComponent<Rigidbody>().velocity = Vector2.zero;
+        player.GetComponent<Rigidbody>().useGravity = false;
+
+        if (currentReality == 1)
+        {
+            currentReality = 2;
+            realityOne.SetActive(false);
+            realityTwo.SetActive(true);
+        }
+        else
+        {
+            currentReality = 1;
+            realityOne.SetActive(true);
+            realityTwo.SetActive(false);
+        }
+        yield return new WaitForSeconds(.5f);
+        player.GetComponent<Rigidbody>().useGravity = true;
+
+    }
+}
