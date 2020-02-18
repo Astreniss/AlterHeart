9af2fc56375de8e69d5c76191a1014bf7d70bb39
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-18 02:41:57 -0600

    Camera now child of player and can turn where mouse faces
    Disabled old camera stuff

--- AlterHeart/Assets/Scripts/CameraController.cs
+++ AlterHeart/Assets/Scripts/CameraController.cs
@@ -10,16 +10,35 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
     private Vector3 startPos;
 
     private void Start()
     {
-        startPos = transform.position;
+        //startPos = transform.position;
+        Cursor.lockState = CursorLockMode.Locked;
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
     void LateUpdate()
     {
         
-        transform.position = target.position + startPos;
+        //transform.position = playerBody.position + startPos;
     }
 }
