011600eda9a6f0c291c2db84cc1ffbd86617c062
probopastor <43503592+probopastor@users.noreply.github.com>
2020-02-18 21:33:46 -0600

    Cross hair functionality

--- AlterHeart/Assets/Scripts/CameraController.cs
+++ AlterHeart/Assets/Scripts/CameraController.cs
@@ -7,9 +7,13 @@
 *****************************************************************************/
 
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CameraController : MonoBehaviour
 {
+    public Image crossHair;
+    public Sprite crossHairUnselected;
+    public Sprite crossHairSelected;
+
     public LayerMask raycastLayer;
     public float spherecastRadius = 1f;
 
@@ -21,6 +25,11 @@ public class CameraController : MonoBehaviour
 
     private void Start()
     {
+        crossHair.sprite = crossHairUnselected;
+
+        //Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        //Cursor.lockState = CursorLockMode.None;
+        //Cursor.visible = false;
         //startPos = transform.position;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -28,7 +37,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
@@ -40,19 +48,35 @@ public class CameraController : MonoBehaviour
         playerBody.Rotate(Vector3.up * mouseX);
 
         RaycastHit hit;
+        ///Ray ray = Camera.main.ScreenPointToRay(crossHair.transform.position);
 
+        
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, raycastLayer))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity, raycastLayer))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            //Debug.Log("Did Hit");
+            crossHair.sprite = crossHairSelected;
+
+            //Debug.DrawRay(ray, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (hit.collider.GetComponent<ButtonController>() != null)
+                {
+                    hit.collider.GetComponent<ButtonController>().PushButton();
+                }
+            }
+        }
+        else
+        {
+            crossHair.sprite = crossHairUnselected;
         }
 
+        /*
         // Spherecast to determine object hit
-        if (Physics.SphereCast(transform.position, spherecastRadius, transform.forward, out hit, Mathf.Infinity, raycastLayer, QueryTriggerInteraction.UseGlobal))
+        if (Physics.SphereCast(Camera.main.ScreenToWorldPoint(Input.mousePosition), spherecastRadius, transform.forward, out hit, Mathf.Infinity, raycastLayer, QueryTriggerInteraction.UseGlobal))
         {
             Debug.Log("Hovering Over Button ");
-
+            
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 if (hit.collider.GetComponent<ButtonController>() != null)
@@ -62,7 +86,7 @@ public class CameraController : MonoBehaviour
             }
 
             //Debug.Log(hit.collider.isTrigger);  
-        }
+        }*/
     }
 
 
